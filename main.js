d5063c4ef837be6a1e6432f730049e79cf144f87
mountt96 <159955699+mountt96@users.noreply.github.com>
2024-05-23 13:53:35 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -23,13 +23,37 @@ function btnClicked() {
   }
 }
 
+let one = 0;
+let two = 0;
+let three = 0;
+let four = 0;
+let five = 0;
+let six = 0;
+var counter = document.getElementById("counter-Out")
 let diceOutputEL = document.getElementById("dice-Output");
 
-document.getElementById("roll").addEventListener("click", rollDice)
+document.getElementById("roll").addEventListener("click", rollDice);
 
-function rollDice(){
-  if(rollDice){
-
-  }  
-
+function rollDice() {
+  var num = Math.round(Math.random() * 6);
+  if (num == 6) {
+    diceOutputEL.src = "img/6.png";
+    six = six + 1;
+  } else if (num == 5) {
+    diceOutputEL.src = "img/5.png";
+    five = five + 1;
+  } else if (num == 4) {
+    diceOutputEL.src = "img/4.png";
+    four = four + 1;
+  } else if (num == 3) {
+    diceOutputEL.src = "img/3.png";
+    three = three + 1;
+  } else if (num == 2) {
+    diceOutputEL.src = "img/2.png";
+    two = two + 1;
+  } else if (num == 1) {
+    diceOutputEL.src = "img/1.png";
+    one = one + 1;
+  }
+  counter-Out.innerHTML = `One:${one} Two:${two} Three:${three} Four:${four} Five:${five} Six:${six}`;
 }
